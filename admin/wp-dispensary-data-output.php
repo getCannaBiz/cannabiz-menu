6116da40a119802e7994718f590ccf087c1abe75
Robert DeVore <deviodigital@gmail.com>
2016-05-04 17:18:06 -0400

    Updated menu item data output
    At this point, it looks like everything that a user can add in the
    backend can be shown on the frontend

--- admin/wp-dispensary-data-output.php
+++ admin/wp-dispensary-data-output.php
@@ -53,28 +53,125 @@ function wpd_data_output_content( $content ) {
 	if ( get_the_term_list( $post->ID, 'ingredients', true ) ) {
 		$wpdingredients = "<tr><td><span>Ingredients:</span></td><td>" . get_the_term_list( $post->ID, 'ingredients', '', ', ', '' ) . "</td></tr>";
 	} else {
-		$wpdingredients = "<tr><td><span>Ingredients</span></td><td></td></tr>";
+		$wpdingredients = "";
 	}
-		if ( in_array( get_post_type(), array( 'flowers','concentrates','edibles' ) ) ) {
-		$original = $content;
-		}
-		if ( in_array( get_post_type(), array( 'flowers','concentrates','edibles' ) ) ) {
-		$content = "<table class='wpdispensary-table single'><tr><td class='wpdispensary-title' colspan='6'>Item Information</td></tr>";
-		}
-		if ( in_array( get_post_type(), array( 'flowers','concentrates' ) ) ) {
-		$content .= $wpdaroma . $wpdflavor . $wpdeffect . $wpdsymptom . $wpdcondition;
-		}
-		if ( 'edibles' === get_post_type() ) {
-		$content .= $wpdingredients;
-		}
-		if ( in_array( get_post_type(), array( 'flowers','concentrates','edibles' ) ) ) {
-		$content .= "</table>";
-		}
 
-		if ( in_array( get_post_type(), array( 'flowers','concentrates','edibles' ) ) ) {
-		$content .= $original;
-		}
-		return $content;
+	
+	if ( get_post_meta( get_the_ID(), '_priceeach', true ) ) {
+		$wpdpriceeach = "<tr class='priceeach'><td><span>Price Each:</span></td><td>$" . get_post_meta(get_the_id(), '_priceeach', true) . "</td></tr>";
+	} else {
+		$wpdpriceeach = "";
+	}		
+
+
+	if ( get_post_meta( get_the_ID(), '_halfgram', true ) ) {
+		$wpdhalfgram = "<td><span>1/2 gram:</span> $" . get_post_meta(get_the_id(), '_halfgram', true) ."</td>";
+	} else {
+		$wpdhalfgram = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_gram', true ) ) {
+		$wpdgram = "<td><span>1 gram:</span> $" . get_post_meta(get_the_id(), '_gram', true) ."</td>";
+	} else {
+		$wpdgram = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_eighth', true ) ) {
+		$wpdeighth = "<td><span>1/8 ounce:</span> $" . get_post_meta(get_the_id(), '_eighth', true) ."</td>";
+	} else {
+		$wpdeighth = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_quarter', true ) ) {
+		$wpdquarter = "<td><span>1/4 ounce:</span> $" . get_post_meta(get_the_id(), '_quarter', true) ."</td>";
+	} else {
+		$wpdquarter = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_halfounce', true ) ) {
+		$wpdhalfounce = "<td><span>1/2 ounce:</span> $" . get_post_meta(get_the_id(), '_halfounce', true) ."</td>";
+	} else {
+		$wpdhalfounce = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_halfounce', true ) ) {
+		$wpdounce = "<td><span>1 ounce:</span> $" . get_post_meta(get_the_id(), '_ounce', true) ."</td>";
+	} else {
+		$wpdounce = "";
+	}
+
+	if ( get_post_meta( get_the_ID(), '_thc', true ) ) {
+		$wpdthc = "<tr><td><span>THC:</span></td><td>" . get_post_meta(get_the_id(), '_thc', true) ."%</td></tr>";
+	} else {
+		$wpdthc = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_cbd', true ) ) {
+		$wpdcbd = "<tr><td><span>CBD:</span></td><td>" . get_post_meta(get_the_id(), '_cbd', true) ."%</td></tr>";
+	} else {
+		$wpdcbd = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_thcmg', true ) ) {
+		$wpdthcmg = "<tr><td><span>mg per serving:</span></td><td>" . get_post_meta(get_the_id(), '_thcmg', true) ."</td></tr>";
+	} else {
+		$wpdthcmg = "";
+	}
+	if ( get_post_meta( get_the_ID(), '_thcservings', true ) ) {
+		$wpdservings = "<tr><td><span>Servings:</span></td><td>" . get_post_meta(get_the_id(), '_thcservings', true) ."</td></tr>";
+	} else {
+		$wpdservings = "";
+	}
+
+	$post_type = get_post_type_object( get_post_type( $post ) );
+
+	/**
+	 * Adding the WP Dispensary menu item data
+	 */
+	if ( in_array( get_post_type(), array( 'flowers','concentrates','edibles' ) ) ) {
+	$original = $content;
+	}
+
+	/**
+	 * Adding Details table
+	 */
+	if ( in_array( get_post_type(), array( 'flowers','concentrates','edibles' ) ) ) {
+	$content = "<table class='wpdispensary-table single'><tr><td class='wpdispensary-title' colspan='6'>" .  $post_type->labels->singular_name . " Details</td></tr>";
+	}
+
+	if ( in_array( get_post_type(), array( 'flowers','concentrates' ) ) ) {
+	$content .= $wpdaroma . $wpdflavor . $wpdeffect . $wpdsymptom . $wpdcondition;
+	}
+
+	if ( 'edibles' === get_post_type() ) {
+	$content .= $wpdingredients . $wpdthcmg . $wpdservings;
+	}
+
+	if ( in_array( get_post_type(), array( 'flowers', 'concentrates', 'prerolls','edibles' ) ) ) {
+	$content .= $wpdthc . $wpdcbd;
+	}
+
+	if ( in_array( get_post_type(), array( 'flowers', 'concentrates', 'prerolls','edibles' ) ) ) {
+	$content .= "</table>";
+	}
+
+	/**
+	 * Adding Pricing table
+	 */
+	if ( in_array( get_post_type(), array( 'flowers','concentrates','prerolls','edibles' ) ) ) {
+	$content .= "<table class='wpdispensary-table single pricing'><tr><td class='wpdispensary-title' colspan='6'>" .  $post_type->labels->singular_name . " Pricing</td></tr>";
+	}
+
+	if ( in_array( get_post_type(), array( 'flowers', 'concentrates' ) ) ) {
+	$content .= "<tr>" . $wpdhalfgram . $wpdgram . $wpdeighth . $wpdquarter . $wpdhalfounce . $wpdounce . "</tr>";
+	}
+
+	if ( in_array( get_post_type(), array( 'prerolls', 'edibles' ) ) ) {
+	$content .= $wpdpriceeach;
+	}
+
+	if ( in_array( get_post_type(), array( 'flowers', 'concentrates', 'prerolls','edibles' ) ) ) {
+	$content .= "</table>";
+	}
+
+	if ( in_array( get_post_type(), array( 'flowers','concentrates','edibles' ) ) ) {
+	$content .= $original;
+	}
+	return $content;
+
 }
 add_filter( 'the_content', 'wpd_data_output_content' );
 
