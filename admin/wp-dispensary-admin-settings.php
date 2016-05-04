53de45713b8f5aa666062bfb67d6058541540622
Robert DeVore <deviodigital@gmail.com>
2016-05-04 15:20:36 -0400

    Small settings page updates

--- admin/wp-dispensary-admin-settings.php
+++ admin/wp-dispensary-admin-settings.php
@@ -13,10 +13,13 @@
  * Creating the menu item
  */
 add_action( 'admin_menu', 'wpd_settings_add_admin_menu' );
-add_action( 'admin_init', 'wpd_settings_settings_init' );
+add_action( 'admin_init', 'wpd_settings_init' );
 
 
-function wpd_settings_add_admin_menu(  ) { 
+/**
+ * Adding the menu page
+ */
+function wpd_settings_add_admin_menu() {
 
 	add_menu_page(
 		'WP Dispensary',
@@ -30,54 +33,38 @@ function wpd_settings_add_admin_menu(  ) {
 }
 
 
-function wpd_settings_settings_init(  ) { 
+/**
+ * Adding settings init
+ */
+
+function wpd_settings_init() {
 
 	register_setting( 'wpdsettings', 'wpd_settings_settings' );
 
 	add_settings_section(
 		'wpd_settings_wpdsettings_section', 
 		__( 'WP Dispensary Settings', 'wp-dispensary' ), 
-		'wpd_settings_settings_section_callback', 
+		'wpd_settings_subtitle_section_callback', 
 		'wpdsettings'
 	);
 
-	add_settings_field( 
+	add_settings_field(
 		'wpd_settings_single_menu_output', 
-		__( 'Hide menu item details in single item output?', 'wp-dispensary' ), 
+		__( 'Hide single menu item details in public display?', 'wp-dispensary' ), 
 		'wpd_settings_single_menu_output_render', 
 		'wpdsettings', 
 		'wpd_settings_wpdsettings_section' 
 	);
 
-	add_settings_field( 
-		'wpd_settings_text_field_2', 
-		__( 'Settings field description', 'wp-dispensary' ), 
-		'wpd_settings_text_field_2_render', 
-		'wpdsettings', 
-		'wpd_settings_wpdsettings_section' 
-	);
-
-	add_settings_field( 
-		'wpd_settings_textarea_field_4', 
-		__( 'Settings field description', 'wp-dispensary' ), 
-		'wpd_settings_textarea_field_4_render', 
-		'wpdsettings', 
-		'wpd_settings_wpdsettings_section' 
-	);
-
-	add_settings_field( 
-		'wpd_settings_select_field_5', 
-		__( 'Settings field description', 'wp-dispensary' ), 
-		'wpd_settings_select_field_5_render', 
-		'wpdsettings', 
-		'wpd_settings_wpdsettings_section' 
-	);
-
-
 }
 
 
-function wpd_settings_single_menu_output_render(  ) { 
+/**
+ * Add Function:
+ * Single Menu Output
+ */
+
+function wpd_settings_single_menu_output_render() {
 
 	$options = get_option( 'wpd_settings_settings' );
 	?>
@@ -87,48 +74,22 @@ function wpd_settings_single_menu_output_render(  ) {
 }
 
 
-function wpd_settings_text_field_2_render(  ) {
+/**
+ * Add Function:
+ * Setting sub-title
+ */
 
-	$options = get_option( 'wpd_settings_settings' );
-	?>
-	<input type='text' name='wpd_settings_settings[wpd_settings_text_field_2]' value='<?php echo $options['wpd_settings_text_field_2']; ?>'>
-	<?php
-
-}
-
-
-function wpd_settings_textarea_field_4_render(  ) { 
-
-	$options = get_option( 'wpd_settings_settings' );
-	?>
-	<textarea cols='40' rows='5' name='wpd_settings_settings[wpd_settings_textarea_field_4]'> 
-		<?php echo $options['wpd_settings_textarea_field_4']; ?>
- 	</textarea>
-	<?php
-
-}
-
-
-function wpd_settings_select_field_5_render(  ) { 
-
-	$options = get_option( 'wpd_settings_settings' );
-	?>
-	<select name='wpd_settings_settings[wpd_settings_select_field_5]'>
-		<option value='1' <?php selected( $options['wpd_settings_select_field_5'], 1 ); ?>>Option 1</option>
-		<option value='2' <?php selected( $options['wpd_settings_select_field_5'], 2 ); ?>>Option 2</option>
-	</select>
-
-<?php
-
-}
-
-
-function wpd_settings_settings_section_callback(  ) { 
+function wpd_settings_subtitle_section_callback(  ) { 
 
 	echo __( '', 'wp-dispensary' );
 
 }
 
+
+/**
+ * Add Function:
+ * Settings option page display
+ */
 
 function wpd_settings_options_page(  ) { 
 
