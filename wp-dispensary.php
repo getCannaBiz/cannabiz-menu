97c5ab79c3d3d86ad0768c88a4dbb5a998f5fea8
Robert DeVore <deviodigital@gmail.com>
2016-11-02 09:50:57 -0400

    Updated to version 1.9

--- wp-dispensary.php
+++ wp-dispensary.php
@@ -11,7 +11,7 @@
  * Plugin Name:       WP Dispensary
  * Plugin URI:        http://www.wpdispensary.com
  * Description:       The complete marijuana dispensary menu solution for WordPress.
- * Version:           1.8
+ * Version:           1.9
  * Author:            WP Dispensary
  * Author URI:        http://www.wpdispensary.com
  * License:           GPL-2.0+
