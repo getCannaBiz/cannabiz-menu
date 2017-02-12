28148d883c82f384e7164980071e55a9528266e5
Robert DeVore <deviodigital@gmail.com>
2017-02-12 18:03:21 -0500

    Added Carousel shortcode

--- admin/wp-dispensary-shortcodes.php
+++ admin/wp-dispensary-shortcodes.php
@@ -1872,3 +1872,389 @@ function wpdispensary_growers_shortcode( $atts ) {
 
 }
 add_shortcode( 'wpd-growers', 'wpdispensary_growers_shortcode' );
+
+
+/**
+ * Carousel Shortcode Fuction
+ *
+ * @access public
+ *
+ * @return string HTML markup.
+ */
+function wpdispensary_carousel_shortcode( $atts ) {
+
+	/* Attributes */
+	extract( shortcode_atts(
+		array(
+			'posts'       => '100',
+			'class'       => '',
+			'name'        => 'show',
+			'info'        => 'show',
+			'thc'         => '',
+			'cbd'         => '',
+			'title'       => 'Dispensary Menu',
+			'category'    => '',
+			'aroma'       => '',
+			'flavor'      => '',
+			'effect'      => '',
+			'symptom'     => '',
+			'condition'   => '',
+			'orderby'     => '',
+			'type'        => array( 'flowers', 'concentrates', 'edibles', 'topicals', 'prerolls', 'growers' ),
+		),
+		$atts
+	) );
+
+	/**
+	 * Code to create shortcode
+	 */
+	$tax_query = array( 'relation' => 'AND' );
+	if ( '' !== $aroma ) {
+			$tax_query[]   = array(
+					'taxonomy' => 'aroma',
+					'field'    => 'slug',
+					'terms'    => $aroma,
+			);
+	}
+	if ( '' !== $flavor ) {
+			$tax_query[]   = array(
+					'taxonomy' => 'flavor',
+					'field'    => 'slug',
+					'terms'    => $flavor,
+			);
+	}
+	if ( '' !== $effect ) {
+			$tax_query[]   = array(
+					'taxonomy' => 'effect',
+					'field'    => 'slug',
+					'terms'    => $effect,
+			);
+	}
+	if ( '' !== $symptom ) {
+			$tax_query[]   = array(
+					'taxonomy' => 'symptom',
+					'field'    => 'slug',
+					'terms'    => $symptom,
+			);
+	}
+	if ( '' !== $condition ) {
+			$tax_query[]   = array(
+					'taxonomy' => 'condition',
+					'field'    => 'slug',
+					'terms'    => $condition,
+			);
+	}
+	if ( '' !== $orderby ) {
+			$order = $orderby;
+			$ordernew = 'ASC';
+	}
+	$args = array(
+		'post_type'          => $type,
+		'posts_per_page'     => $posts,
+		'flowers_category'   => $category,
+		'tax_query'          => $tax_query,
+		'orderby'            => $order,
+		'order'              => $ordernew,
+	);
+
+	$wpdquery = new WP_Query( $args );
+
+	$wpdposts = '<div class="wpdispensary"><h2 class="wpd-title">'. $title .'</h2><div class="wpd-carousel">';
+
+	while ( $wpdquery->have_posts() ) : $wpdquery->the_post();
+
+		$thumbnail_id          = get_post_thumbnail_id();
+		$thumbnail_url_array   = wp_get_attachment_image_src( $thumbnail_id, 'dispensary-image', true );
+		$thumbnail_url         = $thumbnail_url_array[0];
+		$querytitle            = get_the_title();
+
+		/** Get the pricing for Flowers and Concentrates */
+
+		$priceHalfGram    = get_post_meta( get_the_ID(), '_halfgram', true );
+		$priceGram        = get_post_meta( get_the_ID(), '_gram', true );
+		$priceEighth      = get_post_meta( get_the_ID(), '_eighth', true );
+		$priceQuarter     = get_post_meta( get_the_ID(), '_quarter', true );
+		$priceHalfOunce   = get_post_meta( get_the_ID(), '_halfounce', true );
+		$priceOunce       = get_post_meta( get_the_ID(), '_ounce', true );
+
+		$wp_dispensary_options     = get_option( 'wp_dispensary_option_name' ); // Array of All Options
+		$wpd_hide_details          = $wp_dispensary_options['wpd_hide_details']; // hidedetails
+		$wpd_hide_pricing          = $wp_dispensary_options['wpd_hide_pricing']; // hidepricing
+		$wpd_content_placement     = $wp_dispensary_options['wpd_content_placement']; // movecontent
+		if ( null === $wp_dispensary_options['wpd_currency'] ) {
+			$wpd_currency = 'USD';
+		} else {
+			$wpd_currency = $wp_dispensary_options['wpd_currency'];
+		}
+		if ( null === $wp_dispensary_options['wpd_cost_phrase'] ) {
+			$wpd_cost_phrase = 'Price';
+		} else {
+			$wpd_cost_phrase = $wp_dispensary_options['wpd_cost_phrase']; // costphrase
+		}
+
+		$currency_symbols = array(
+			'AED' => '&#1583;.&#1573;', // ?
+			'AFN' => '&#65;&#102;',
+			'ALL' => '&#76;&#101;&#107;',
+			'AMD' => '',
+			'ANG' => '&#402;',
+			'AOA' => '&#75;&#122;', // ?
+			'ARS' => '&#36;',
+			'AUD' => '&#36;',
+			'AWG' => '&#402;',
+			'AZN' => '&#1084;&#1072;&#1085;',
+			'BAM' => '&#75;&#77;',
+			'BBD' => '&#36;',
+			'BDT' => '&#2547;', // ?
+			'BGN' => '&#1083;&#1074;',
+			'BHD' => '.&#1583;.&#1576;', // ?
+			'BIF' => '&#70;&#66;&#117;', // ?
+			'BMD' => '&#36;',
+			'BND' => '&#36;',
+			'BOB' => '&#36;&#98;',
+			'BRL' => '&#82;&#36;',
+			'BSD' => '&#36;',
+			'BTN' => '&#78;&#117;&#46;', // ?
+			'BWP' => '&#80;',
+			'BYR' => '&#112;&#46;',
+			'BZD' => '&#66;&#90;&#36;',
+			'CAD' => '&#36;',
+			'CDF' => '&#70;&#67;',
+			'CHF' => '&#67;&#72;&#70;',
+			'CLF' => '', // ?
+			'CLP' => '&#36;',
+			'CNY' => '&#165;',
+			'COP' => '&#36;',
+			'CRC' => '&#8353;',
+			'CUP' => '&#8396;',
+			'CVE' => '&#36;', // ?
+			'CZK' => '&#75;&#269;',
+			'DJF' => '&#70;&#100;&#106;', // ?
+			'DKK' => '&#107;&#114;',
+			'DOP' => '&#82;&#68;&#36;',
+			'DZD' => '&#1583;&#1580;', // ?
+			'EGP' => '&#163;',
+			'ETB' => '&#66;&#114;',
+			'EUR' => '&#8364;',
+			'FJD' => '&#36;',
+			'FKP' => '&#163;',
+			'GBP' => '&#163;',
+			'GEL' => '&#4314;', // ?
+			'GHS' => '&#162;',
+			'GIP' => '&#163;',
+			'GMD' => '&#68;', // ?
+			'GNF' => '&#70;&#71;', // ?
+			'GTQ' => '&#81;',
+			'GYD' => '&#36;',
+			'HKD' => '&#36;',
+			'HNL' => '&#76;',
+			'HRK' => '&#107;&#110;',
+			'HTG' => '&#71;', // ?
+			'HUF' => '&#70;&#116;',
+			'IDR' => '&#82;&#112;',
+			'ILS' => '&#8362;',
+			'INR' => '&#8377;',
+			'IQD' => '&#1593;.&#1583;', // ?
+			'IRR' => '&#65020;',
+			'ISK' => '&#107;&#114;',
+			'JEP' => '&#163;',
+			'JMD' => '&#74;&#36;',
+			'JOD' => '&#74;&#68;', // ?
+			'JPY' => '&#165;',
+			'KES' => '&#75;&#83;&#104;', // ?
+			'KGS' => '&#1083;&#1074;',
+			'KHR' => '&#6107;',
+			'KMF' => '&#67;&#70;', // ?
+			'KPW' => '&#8361;',
+			'KRW' => '&#8361;',
+			'KWD' => '&#1583;.&#1603;', // ?
+			'KYD' => '&#36;',
+			'KZT' => '&#1083;&#1074;',
+			'LAK' => '&#8365;',
+			'LBP' => '&#163;',
+			'LKR' => '&#8360;',
+			'LRD' => '&#36;',
+			'LSL' => '&#76;', // ?
+			'LTL' => '&#76;&#116;',
+			'LVL' => '&#76;&#115;',
+			'LYD' => '&#1604;.&#1583;', // ?
+			'MAD' => '&#1583;.&#1605;.', // ?
+			'MDL' => '&#76;',
+			'MGA' => '&#65;&#114;', // ?
+			'MKD' => '&#1076;&#1077;&#1085;',
+			'MMK' => '&#75;',
+			'MNT' => '&#8366;',
+			'MOP' => '&#77;&#79;&#80;&#36;', // ?
+			'MRO' => '&#85;&#77;', // ?
+			'MUR' => '&#8360;', // ?
+			'MVR' => '.&#1923;', // ?
+			'MWK' => '&#77;&#75;',
+			'MXN' => '&#36;',
+			'MYR' => '&#82;&#77;',
+			'MZN' => '&#77;&#84;',
+			'NAD' => '&#36;',
+			'NGN' => '&#8358;',
+			'NIO' => '&#67;&#36;',
+			'NOK' => '&#107;&#114;',
+			'NPR' => '&#8360;',
+			'NZD' => '&#36;',
+			'OMR' => '&#65020;',
+			'PAB' => '&#66;&#47;&#46;',
+			'PEN' => '&#83;&#47;&#46;',
+			'PGK' => '&#75;', // ?
+			'PHP' => '&#8369;',
+			'PKR' => '&#8360;',
+			'PLN' => '&#122;&#322;',
+			'PYG' => '&#71;&#115;',
+			'QAR' => '&#65020;',
+			'RON' => '&#108;&#101;&#105;',
+			'RSD' => '&#1044;&#1080;&#1085;&#46;',
+			'RUB' => '&#1088;&#1091;&#1073;',
+			'RWF' => '&#1585;.&#1587;',
+			'SAR' => '&#65020;',
+			'SBD' => '&#36;',
+			'SCR' => '&#8360;',
+			'SDG' => '&#163;', // ?
+			'SEK' => '&#107;&#114;',
+			'SGD' => '&#36;',
+			'SHP' => '&#163;',
+			'SLL' => '&#76;&#101;', // ?
+			'SOS' => '&#83;',
+			'SRD' => '&#36;',
+			'STD' => '&#68;&#98;', // ?
+			'SVC' => '&#36;',
+			'SYP' => '&#163;',
+			'SZL' => '&#76;', // ?
+			'THB' => '&#3647;',
+			'TJS' => '&#84;&#74;&#83;', // ? TJS (guess)
+			'TMT' => '&#109;',
+			'TND' => '&#1583;.&#1578;',
+			'TOP' => '&#84;&#36;',
+			'TRY' => '&#8356;', // New Turkey Lira (old symbol used)
+			'TTD' => '&#36;',
+			'TWD' => '&#78;&#84;&#36;',
+			'TZS' => '',
+			'UAH' => '&#8372;',
+			'UGX' => '&#85;&#83;&#104;',
+			'USD' => '&#36;',
+			'UYU' => '&#36;&#85;',
+			'UZS' => '&#1083;&#1074;',
+			'VEF' => '&#66;&#115;',
+			'VND' => '&#8363;',
+			'VUV' => '&#86;&#84;',
+			'WST' => '&#87;&#83;&#36;',
+			'XAF' => '&#70;&#67;&#70;&#65;',
+			'XCD' => '&#36;',
+			'XDR' => '',
+			'XOF' => '',
+			'XPF' => '&#70;',
+			'YER' => '&#65020;',
+			'ZAR' => '&#82;',
+			'ZMK' => '&#90;&#75;', // ?
+			'ZWL' => '&#90;&#36;',
+		);
+
+		if ( '' === $priceGram && '' === $priceEighth && '' === $priceQuarter && '' === $priceHalfOunce && '' === $priceOunce ) {
+			$pricing = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_halfgram', true ) . ' per half gram';
+		} elseif ( '' === $priceHalfGram && '' === $priceEighth && '' === $priceQuarter && '' === $priceHalfOunce && '' === $priceOunce ) {
+			$pricing = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_gram', true ) . ' per gram';
+		} elseif ( '' === $priceHalfGram && '' === $priceGram && '' === $priceQuarter && '' === $priceHalfOunce && '' === $priceOunce ) {
+			$pricing = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_eighth', true ) . ' per eighth';
+		} elseif ( '' === $priceHalfGram && '' === $priceGram && '' === $priceEighth && '' === $priceHalfOunce && '' === $priceOunce ) {
+			$pricing = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_quarter', true ) . ' per quarter ounce';
+		} elseif ( '' === $priceHalfGram && '' === $priceGram && '' === $priceEighth && '' === $priceQuarter && '' === $priceOunce ) {
+			$pricing = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_halfounce', true ) . ' per half ounce';
+		} elseif ( '' === $priceHalfGram && '' === $priceGram && '' === $priceEighth && '' === $priceQuarter && '' === $priceHalfOunce ) {
+			$pricing = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_ounce', true ) . ' per ounce';
+		} else {
+			$pricing = '';
+		}
+
+		if ( get_post_meta( get_the_ID(), '_halfgram', true ) ) {
+			$pricinglow = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_halfgram', true );
+		} elseif ( get_post_meta( get_the_ID(), '_gram', true ) ) {
+			$pricinglow = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_gram', true );
+		} elseif ( get_post_meta( get_the_ID(), '_eighth', true ) ) {
+			$pricinglow = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_eighth', true );
+		} elseif ( get_post_meta( get_the_ID(), '_quarter', true ) ) {
+			$pricinglow = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_quarter', true );
+		} elseif ( get_post_meta( get_the_ID(), '_halfounce', true ) ) {
+			$pricinglow = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_halfounce', true );
+		}
+		$pricingsep = ' - ';
+		if ( get_post_meta( get_the_ID(), '_ounce', true ) ) {
+			$pricinghigh = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_ounce', true );
+		} elseif ( get_post_meta( get_the_ID(), '_halfounce', true ) ) {
+			$pricinghigh = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_halfounce', true );
+		} elseif ( get_post_meta( get_the_ID(), '_quarter', true ) ) {
+			$pricinghigh = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_quarter', true );
+		} elseif ( get_post_meta( get_the_ID(), '_eighth', true ) ) {
+			$pricinghigh = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_eighth', true );
+		} elseif ( get_post_meta( get_the_ID(), '_gram', true ) ) {
+			$pricinghigh = $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_gram', true );
+		}
+
+		if ( get_post_meta( get_the_ID(), '_thc', true ) ) {
+			$thcinfo = '<span class="wpd-productinfo"><strong>THC: </strong>' . get_post_meta( get_the_id(), '_thc', true ) .'%</span>';
+		} else {
+			$thcinfo = '';
+		}
+
+		if ( get_post_meta( get_the_ID(), '_cbd', true ) ) {
+			$cbdinfo = '<span class="wpd-productinfo"><strong>CBD: </strong>' . get_post_meta( get_the_id(), '_cbd', true ) .'%</span>';
+		} else {
+			$cbdinfo = '';
+		}
+
+		/** Check shortcode options input by user */
+
+		if ( 'show' === $name ) {
+			$showname = '<p><strong><a href="' . get_permalink() . '">' . $querytitle . '</a></strong></p>';
+		} else {
+			$showname = '';
+		}
+
+		if ( 'show' === $info ) {
+			if ( empty( $pricing ) ) {
+				$showinfo = '<span class="wpd-productinfo"><strong>'. $wpd_cost_phrase .':</strong> ' . $pricinglow . '' . $pricingsep . '' . $pricinghigh . '</span>';
+			} else {
+				$showinfo = '<span class="wpd-productinfo"><strong>'. $wpd_cost_phrase .':</strong> ' . $pricing . '</span>';
+			}
+		} else {
+			$showinfo = '';
+		}
+
+		if ( 'show' === $thc ) {
+			$showthc = $thcinfo;
+		} else {
+			$showthc = '';
+		}
+
+		if ( 'show' === $cbd ) {
+			$showcbd = $cbdinfo;
+		} else {
+			$showcbd = '';
+		}
+
+		/** Shortcode display */
+
+		ob_start();
+			do_action( 'wpd_shortcode_inside_top' );
+			$wpd_shortcode_inside_top = ob_get_contents();
+		ob_end_clean();
+
+		ob_start();
+			do_action( 'wpd_shortcode_inside_bottom' );
+			$wpd_shortcode_inside_bottom = ob_get_contents();
+		ob_end_clean();
+
+		$wpdposts .= '<div class="carousel-item ' . $class .'">'. $wpd_shortcode_inside_top .'<a href="' . get_permalink() . '"><img src="' . $thumbnail_url . '" alt="Menu - Flower" /></a>'. $showname .''. $showinfo .''. $showthc .''. $showcbd .''. $wpd_shortcode_inside_bottom .'</div>';
+
+	endwhile;
+
+	wp_reset_postdata();
+
+	return $wpdposts . '</div></div>';
+
+}
+add_shortcode( 'wpd-carousel', 'wpdispensary_carousel_shortcode' );
