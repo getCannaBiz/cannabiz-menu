0a276168d6fecf3d0e79faa043d84dd2263e1588
Robert DeVore <deviodigital@gmail.com>
2017-05-09 02:27:04 -0400

    Cleaned up code so it's easier to read

--- admin/class-wp-dispensary-admin.php
+++ admin/class-wp-dispensary-admin.php
@@ -119,16 +119,16 @@ class WP_Dispensary_Admin {
  */
 function wpdispensary_right_now_content_table_end() {
 	$args = array(
-		'public' => true,
-		'_builtin' => false,
+		'public'     => true,
+		'_builtin'   => false,
 	);
-	$output = 'object';
-	$operator = 'and';
+	$output     = 'object';
+	$operator   = 'and';
 	$post_types = get_post_types( $args , $output , $operator );
 	foreach ( $post_types as $post_type ) {
-		$num_posts = wp_count_posts( $post_type->name );
-		$num = number_format_i18n( $num_posts->publish );
-		$text = _n( $post_type->labels->singular_name, $post_type->labels->name , intval( $num_posts->publish ) );
+		$num_posts     = wp_count_posts( $post_type->name );
+		$num           = number_format_i18n( $num_posts->publish );
+		$text          = _n( $post_type->labels->singular_name, $post_type->labels->name , intval( $num_posts->publish ) );
 		if ( current_user_can( 'edit_posts' ) ) {
 			$cpt_name = $post_type->name;
 		}
