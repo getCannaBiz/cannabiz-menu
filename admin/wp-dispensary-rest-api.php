100dfeabb6e6b737f424a74ed1278a9a240330ee
Robert DeVore <deviodigital@gmail.com>
2017-08-19 00:11:31 -0400

    Removed the halfgram price in Flowers

--- admin/wp-dispensary-rest-api.php
+++ admin/wp-dispensary-rest-api.php
@@ -274,7 +274,7 @@ add_action( 'rest_api_init', 'slug_register_prices' );
  * Registering Prices
  */
 function slug_register_prices() {
-	$productsizes = array( '_halfgram', '_gram', '_eighth', '_quarter', '_halfounce', '_ounce' );
+	$productsizes = array( '_gram', '_eighth', '_quarter', '_halfounce', '_ounce' );
 	foreach ( $productsizes as $size ) {
 		register_rest_field(
 			array( 'flowers' ),
