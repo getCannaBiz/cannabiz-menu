c6a5691a3648ce253a29d10695b0cf591aa6df30
Robert DeVore <deviodigital@gmail.com>
2017-11-02 11:26:36 -0400

    Changed "per roll" to "each" for pre-roll shortcode prices
    Since dispensaries sell multiple pre-rolls in a "pack", changing the
    wording from "per roll" to "each" makes the price display work no matter
    how many pre-rolls are being sold with each menu item.

--- admin/wp-dispensary-shortcodes.php
+++ admin/wp-dispensary-shortcodes.php
@@ -1547,7 +1547,7 @@ function wpdispensary_prerolls_shortcode( $atts ) {
 		 */
 
 		if ( get_post_meta( get_the_ID(), '_priceeach', true ) ) {
-			$pricingeach = '<strong>' . $wpd_cost_phrase . ':</strong> ' . $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_priceeach', true ) . ' per roll';
+			$pricingeach = '<strong>' . $wpd_cost_phrase . ':</strong> ' . $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_priceeach', true ) . ' each';
 		}
 
 		/** Check shortcode options input by user */
@@ -2726,7 +2726,7 @@ function wpdispensary_carousel_shortcode( $atts ) {
 		 */
 
 		if ( get_post_meta( get_the_ID(), '_priceeach', true ) ) {
-			$pricingeach = '<strong>' . $wpd_cost_phrase . ':</strong> ' . $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_priceeach', true ) . ' per roll';
+			$pricingeach = '<strong>' . $wpd_cost_phrase . ':</strong> ' . $currency_symbols[ $wpd_currency ] . '' . get_post_meta( get_the_id(), '_priceeach', true ) . ' each';
 		}
 
 		/*
