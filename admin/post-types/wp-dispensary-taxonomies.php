3e6640210179fc4f9cdacfa765f9a9472df023e5
Devio Digital <deviodigital@gmail.com>
2017-11-20 12:18:50 -0500

    WordPress Coding Standards Updates #1

--- admin/post-types/wp-dispensary-taxonomies.php
+++ admin/post-types/wp-dispensary-taxonomies.php
@@ -332,18 +332,18 @@ add_action( 'init', 'wpdispensary_flowercategory', 0 );
 function wpdispensary_flowercategory() {
 
 	$labels = array(
-		'name'                       => _x( 'Flower Categories', 'taxonomy general name' ),
-		'singular_name'              => _x( 'Flower Category', 'taxonomy singular name' ),
-		'search_items'               => __( 'Search Categories' ),
-		'all_items'                  => __( 'All Categories' ),
-		'parent_item'                => __( 'Parent Category' ),
-		'parent_item_colon'          => __( 'Parent Category:' ),
-		'edit_item'                  => __( 'Edit Category' ),
-		'update_item'                => __( 'Update Category' ),
-		'add_new_item'               => __( 'Add New Category' ),
-		'new_item_name'              => __( 'New Category Name' ),
-		'not_found'                  => 'No categories found',
-		'menu_name'                  => __( 'Categories' ),
+		'name'              => _x( 'Flower Categories', 'taxonomy general name' ),
+		'singular_name'     => _x( 'Flower Category', 'taxonomy singular name' ),
+		'search_items'      => __( 'Search Categories' ),
+		'all_items'         => __( 'All Categories' ),
+		'parent_item'       => __( 'Parent Category' ),
+		'parent_item_colon' => __( 'Parent Category:' ),
+		'edit_item'         => __( 'Edit Category' ),
+		'update_item'       => __( 'Update Category' ),
+		'add_new_item'      => __( 'Add New Category' ),
+		'new_item_name'     => __( 'New Category Name' ),
+		'not_found'         => 'No categories found',
+		'menu_name'         => __( 'Categories' ),
 	);
 
 	register_taxonomy( 'flowers_category', array( 'flowers', 'prerolls' ), array(
