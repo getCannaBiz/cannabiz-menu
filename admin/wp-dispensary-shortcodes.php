8cffa3af290045f61daaf1af488810661da74b9b
Robert DeVore <deviodigital@gmail.com>
2017-12-15 17:36:20 -0500

    Added "full" size image check in Topicals shortcode

--- admin/wp-dispensary-shortcodes.php
+++ admin/wp-dispensary-shortcodes.php
@@ -1934,12 +1934,16 @@ function wpdispensary_topicals_shortcode( $atts ) {
 		}
 
 		if ( 'show' === $image ) {
-			if ( null !== $thumbnail_url ) {
+			if ( null === $thumbnail_url && 'full' === $imagesize ) {
+				$wpd_shortcodes_default_image = site_url() . '/wp-content/plugins/wp-dispensary/public/images/wpd-large.jpg';
+				$defaultimg                   = apply_filters( 'wpd_shortcodes_default_image', $wpd_shortcodes_default_image );
+				$showimage                    = '<a href="' . get_permalink() . '"><img src="' . $defaultimg . '" alt="Menu - Topical" /></a>';
+			} elseif ( null !== $thumbnail_url ) {
 				$showimage = '<a href="' . get_permalink() . '"><img src="' . $thumbnail_url . '" alt="Menu - Topical" /></a>';
 			} else {
-				$wpd_default_image = site_url() . '/wp-content/plugins/wp-dispensary/public/images/' . $imagesize . '.jpg';
-				$defaultimg        = apply_filters( 'wpd_default_image', $wpd_default_image );
-				$showimage         = '<a href="' . get_permalink() . '"><img src="' . $defaultimg . '" alt="Menu - Topical" /></a>';
+				$wpd_shortcodes_default_image = site_url() . '/wp-content/plugins/wp-dispensary/public/images/' . $imagesize . '.jpg';
+				$defaultimg                   = apply_filters( 'wpd_shortcodes_default_image', $wpd_shortcodes_default_image );
+				$showimage                    = '<a href="' . get_permalink() . '"><img src="' . $defaultimg . '" alt="Menu - Topical" /></a>';
 			}
 		} else {
 			$showimage = '';
