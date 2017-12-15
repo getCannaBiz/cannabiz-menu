fde9729510e35d6f86e117939f56d46cbafc8ec4
Robert DeVore <deviodigital@gmail.com>
2017-12-15 17:36:55 -0500

    Added "full" size image check in Edibles shortcode

--- admin/wp-dispensary-shortcodes.php
+++ admin/wp-dispensary-shortcodes.php
@@ -1259,12 +1259,16 @@ function wpdispensary_edibles_shortcode( $atts ) {
 		}
 
 		if ( 'show' === $image ) {
-			if ( null !== $thumbnail_url ) {
+			if ( null === $thumbnail_url && 'full' === $imagesize ) {
+				$wpd_shortcodes_default_image = site_url() . '/wp-content/plugins/wp-dispensary/public/images/wpd-large.jpg';
+				$defaultimg                   = apply_filters( 'wpd_shortcodes_default_image', $wpd_shortcodes_default_image );
+				$showimage                    = '<a href="' . get_permalink() . '"><img src="' . $defaultimg . '" alt="Menu - Edible" /></a>';
+			} elseif ( null !== $thumbnail_url ) {
 				$showimage = '<a href="' . get_permalink() . '"><img src="' . $thumbnail_url . '" alt="Menu - Edible" /></a>';
 			} else {
-				$wpd_default_image = site_url() . '/wp-content/plugins/wp-dispensary/public/images/' . $imagesize . '.jpg';
-				$defaultimg        = apply_filters( 'wpd_default_image', $wpd_default_image );
-				$showimage         = '<a href="' . get_permalink() . '"><img src="' . $defaultimg . '" alt="Menu - Edible" /></a>';
+				$wpd_shortcodes_default_image = site_url() . '/wp-content/plugins/wp-dispensary/public/images/' . $imagesize . '.jpg';
+				$defaultimg                   = apply_filters( 'wpd_shortcodes_default_image', $wpd_shortcodes_default_image );
+				$showimage                    = '<a href="' . get_permalink() . '"><img src="' . $defaultimg . '" alt="Menu - Edible" /></a>';
 			}
 		} else {
 			$showimage = '';
