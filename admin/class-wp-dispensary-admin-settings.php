d7ec5285cab89f0666d54ec06546dca4a698998c
Robert DeVore <deviodigital@gmail.com>
2018-03-21 16:05:39 -0400

    Updated label details

--- admin/class-wp-dispensary-admin-settings.php
+++ admin/class-wp-dispensary-admin-settings.php
@@ -525,9 +525,9 @@ class WPD_ADMIN_SETTINGS {
 	function callback_checkbox( $args ) {
 	    $value = esc_attr( $this->get_option( $args['id'], $args['section'], $args['std'] ) );
 	    $html  = '<fieldset>';
-	    $html .= sprintf( '<label for="wpuf-%1$s[%2$s]">', $args['section'], $args['id'] );
+	    $html .= sprintf( '<label for="wpd-settings-%1$s[%2$s]">', $args['section'], $args['id'] );
 	    $html .= sprintf( '<input type="hidden" name="%1$s[%2$s]" value="off" />', $args['section'], $args['id'] );
-	    $html .= sprintf( '<input type="checkbox" class="checkbox" id="wpuf-%1$s[%2$s]" name="%1$s[%2$s]" value="on" %3$s />', $args['section'], $args['id'], checked( $value, 'on', false ) );
+	    $html .= sprintf( '<input type="checkbox" class="checkbox" id="wpd-settings-%1$s[%2$s]" name="%1$s[%2$s]" value="on" %3$s />', $args['section'], $args['id'], checked( $value, 'on', false ) );
 	    $html .= sprintf( '%1$s</label>', $args['desc'] );
 	    $html .= '</fieldset>';
 	    echo $html;
@@ -542,8 +542,8 @@ class WPD_ADMIN_SETTINGS {
 	    $html  = '<fieldset>';
 	    foreach ( $args['options'] as $key => $label ) {
 	        $checked = isset( $value[$key] ) ? $value[$key] : '0';
-	        $html .= sprintf( '<label for="wpuf-%1$s[%2$s][%3$s]">', $args['section'], $args['id'], $key );
-	        $html .= sprintf( '<input type="checkbox" class="checkbox" id="wpuf-%1$s[%2$s][%3$s]" name="%1$s[%2$s][%3$s]" value="%3$s" %4$s />', $args['section'], $args['id'], $key, checked( $checked, $key, false ) );
+	        $html .= sprintf( '<label for="wpd-settings-%1$s[%2$s][%3$s]">', $args['section'], $args['id'], $key );
+	        $html .= sprintf( '<input type="checkbox" class="checkbox" id="wpd-settings-%1$s[%2$s][%3$s]" name="%1$s[%2$s][%3$s]" value="%3$s" %4$s />', $args['section'], $args['id'], $key, checked( $checked, $key, false ) );
 	        $html .= sprintf( '%1$s</label><br>',  $label );
 	    }
 	    $html .= $this->get_field_description( $args );
@@ -559,8 +559,8 @@ class WPD_ADMIN_SETTINGS {
 	    $value = $this->get_option( $args['id'], $args['section'], $args['std'] );
 	    $html  = '<fieldset>';
 	    foreach ( $args['options'] as $key => $label ) {
-	        $html .= sprintf( '<label for="wpuf-%1$s[%2$s][%3$s]">',  $args['section'], $args['id'], $key );
-	        $html .= sprintf( '<input type="radio" class="radio" id="wpuf-%1$s[%2$s][%3$s]" name="%1$s[%2$s]" value="%3$s" %4$s />', $args['section'], $args['id'], $key, checked( $value, $key, false ) );
+	        $html .= sprintf( '<label for="wpd-settings-%1$s[%2$s][%3$s]">',  $args['section'], $args['id'], $key );
+	        $html .= sprintf( '<input type="radio" class="radio" id="wpd-settings-%1$s[%2$s][%3$s]" name="%1$s[%2$s]" value="%3$s" %4$s />', $args['section'], $args['id'], $key, checked( $value, $key, false ) );
 	        $html .= sprintf( '%1$s</label><br>', $label );
 	    }
 	    $html .= $this->get_field_description( $args );
