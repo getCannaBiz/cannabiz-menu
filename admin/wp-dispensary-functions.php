06c45946b1b6cd17a2f974639527ee74ebb51511
Robert DeVore <deviodigital@gmail.com>
2018-03-21 12:06:02 -0400

    Added new file for custom functions

---
+++ admin/wp-dispensary-functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Adding the Functions that are used throughout
+ * various areas of the plugin
+ *
+ * @link       https://www.wpdispensary.com
+ * @since      2.0.0
+ *
+ * @package    WP_Dispensary
+ * @subpackage WP_Dispensary/admin
+ */
