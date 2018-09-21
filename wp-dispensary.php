66d9350c2bbc500f8058b6e12ba59f981d04942c
Robert DeVore <deviodigital@gmail.com>
2018-09-21 14:37:42 -0400

    :ok_hand: IMPROVE: Added Go Pro link to Plugins page

--- wp-dispensary.php
+++ wp-dispensary.php
@@ -98,8 +98,10 @@ run_wp_dispensary();
  * @return array updatead array of links related to the plugin.
  */
 function wpd_settings_link( $links ) {
+	$pro_link      = '<a href="https://www.wpdispensary.com/product/pro-package/" target="_blank" style="font-weight:700;">Go Pro</a>';
 	$settings_link = '<a href="admin.php?page=wpd-settings">Settings</a>';
 	array_unshift( $links, $settings_link );
+	array_unshift( $links, $pro_link );
 	return $links;
 }
 
