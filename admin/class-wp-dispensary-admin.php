15492294acee9f68736ada2546c60a395a5ff565
Robert DeVore <deviodigital@gmail.com>
2020-03-11 22:04:50 -0400

    :ok_hand: IMPROVE: General code cleanup

--- admin/class-wp-dispensary-admin.php
+++ admin/class-wp-dispensary-admin.php
@@ -65,18 +65,6 @@ class WP_Dispensary_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WP_Dispensary_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WP_Dispensary_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-dispensary-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -90,18 +78,6 @@ class WP_Dispensary_Admin {
 	 * @return void
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WP_Dispensary_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WP_Dispensary_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-dispensary-admin.js', array( 'jquery' ), $this->version, false );
 
