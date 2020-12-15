3dea32f1a1dbb46d951f02f511481fd3b2e9d67f
Robert DeVore <deviodigital@gmail.com>
2020-12-14 20:19:21 -0500

    :ok_hand: IMPROVE: Updated body classes for WPD menu and products

--- public/class-wp-dispensary-public.php
+++ public/class-wp-dispensary-public.php
@@ -64,21 +64,7 @@ class WP_Dispensary_Public {
 	 * @return void
 	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WP_Dispensary_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WP_Dispensary_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-dispensary-public.min.css', array(), $this->version, 'all' );
-
 	}
 
 	/**
@@ -89,21 +75,7 @@ class WP_Dispensary_Public {
 	 * @return void
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in WP_Dispensary_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The WP_Dispensary_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-dispensary-public.js', array( 'jquery' ), $this->version, false );
-
 	}
 }
 
@@ -129,29 +101,9 @@ function wp_dispensary_body_class( $classes ) {
 		$classes[] = 'wp-dispensary';
 		$classes[] = 'dispensary-menu';
 	}
-	if ( is_singular( 'flowers' ) || is_post_type_archive( 'flowers' ) ) {
+	if ( is_singular( 'products' ) || is_post_type_archive( 'products' ) ) {
 		$classes[] = 'wp-dispensary';
-		$classes[] = 'wpd-flowers';
-	}
-	if ( is_singular( 'concentrates' ) || is_post_type_archive( 'concentrates' ) ) {
-		$classes[] = 'wp-dispensary';
-		$classes[] = 'wpd-concentrates';
-	}
-	if ( is_singular( 'edibles' ) || is_post_type_archive( 'edibles' ) ) {
-		$classes[] = 'wp-dispensary';
-		$classes[] = 'wpd-edibles';
-	}
-	if ( is_singular( 'prerolls' ) || is_post_type_archive( 'prerolls' ) ) {
-		$classes[] = 'wp-dispensary';
-		$classes[] = 'wpd-prerolls';
-	}
-	if ( is_singular( 'topicals' ) || is_post_type_archive( 'topicals' ) ) {
-		$classes[] = 'wp-dispensary';
-		$classes[] = 'wpd-topicals';
-	}
-	if ( is_singular( 'growers' ) || is_post_type_archive( 'growers' ) ) {
-		$classes[] = 'wp-dispensary';
-		$classes[] = 'wpd-growers';
+		$classes[] = 'wpd-products';
 	}
 
 	return $classes; 
