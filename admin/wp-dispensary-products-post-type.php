4ab0603fb321a6000c457977ecc765bcb7f69a9f
Robert DeVore <deviodigital@gmail.com>
2021-08-29 20:29:12 -0400

    :ok_hand: IMPROVE: Updated post type singular name

--- admin/wp-dispensary-products-post-type.php
+++ admin/wp-dispensary-products-post-type.php
@@ -34,6 +34,11 @@ function wp_dispensary_products_post_type() {
 	// Capitalize first letter of new slug.
 	$wpd_products_slug_cap = ucfirst( $wpd_products_slug );
 
+	// Singular slug.
+	if ( 's' == substr( $wpd_products_slug_cap, -1 ) ) {
+	    $wpd_products_slug_singular = substr( $wpd_products_slug_cap, 0, -1 );
+	}
+
 	$rewrite = array(
 		'slug'       => $wpd_products_slug,
 		'with_front' => true,
@@ -43,18 +48,18 @@ function wp_dispensary_products_post_type() {
 
 	$labels = array(
 		'name'                  => sprintf( esc_html__( '%s', 'Post Type General Name', 'wp-dispensary' ), $wpd_products_slug_cap ),
-		'singular_name'         => sprintf( esc_html__( '%s', 'Post Type Singular Name', 'wp-dispensary' ), $wpd_products_slug_cap ),
+		'singular_name'         => sprintf( esc_html__( '%s', 'Post Type Singular Name', 'wp-dispensary' ), $wpd_products_slug_singular ),
 		'menu_name'             => sprintf( esc_html__( '%s', 'wp-dispensary' ), $wpd_products_slug_cap ),
 		'name_admin_bar'        => sprintf( esc_html__( '%s', 'wp-dispensary' ), $wpd_products_slug_cap ),
 		'archives'              => sprintf( esc_html__( '%s Archives', 'wp-dispensary' ), $wpd_products_slug_cap ),
-		'parent_item_colon'     => sprintf( esc_html__( 'Parent %s:', 'wp-dispensary' ), $wpd_products_slug_cap ),
+		'parent_item_colon'     => sprintf( esc_html__( 'Parent %s:', 'wp-dispensary' ), $wpd_products_slug_singular ),
 		'all_items'             => sprintf( esc_html__( 'All %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
-		'add_new_item'          => sprintf( esc_html__( 'Add New %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
+		'add_new_item'          => sprintf( esc_html__( 'Add New %s', 'wp-dispensary' ), $wpd_products_slug_singular ),
 		'add_new'               => esc_html__( 'Add New', 'wp-dispensary' ),
-		'new_item'              => sprintf( esc_html__( 'New %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
-		'edit_item'             => sprintf( esc_html__( 'Edit %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
-		'update_item'           => sprintf( esc_html__( 'Update %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
-		'view_item'             => sprintf( esc_html__( 'View %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
+		'new_item'              => sprintf( esc_html__( 'New %s', 'wp-dispensary' ), $wpd_products_slug_singular ),
+		'edit_item'             => sprintf( esc_html__( 'Edit %s', 'wp-dispensary' ), $wpd_products_slug_singular ),
+		'update_item'           => sprintf( esc_html__( 'Update %s', 'wp-dispensary' ), $wpd_products_slug_singular ),
+		'view_item'             => sprintf( esc_html__( 'View %s', 'wp-dispensary' ), $wpd_products_slug_singular ),
 		'search_items'          => sprintf( esc_html__( 'Search %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
 		'not_found'             => esc_html__( 'Not found', 'wp-dispensary' ),
 		'not_found_in_trash'    => esc_html__( 'Not found in Trash', 'wp-dispensary' ),
@@ -62,8 +67,8 @@ function wp_dispensary_products_post_type() {
 		'set_featured_image'    => esc_html__( 'Set featured image', 'wp-dispensary' ),
 		'remove_featured_image' => esc_html__( 'Remove featured image', 'wp-dispensary' ),
 		'use_featured_image'    => esc_html__( 'Use as featured image', 'wp-dispensary' ),
-		'insert_into_item'      => sprintf( esc_html__( 'Insert into %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
-		'uploaded_to_this_item' => sprintf( esc_html__( 'Uploaded to this %s', 'wp-dispensary' ), $wpd_products_slug_cap ),
+		'insert_into_item'      => sprintf( esc_html__( 'Insert into %s', 'wp-dispensary' ), $wpd_products_slug_singular ),
+		'uploaded_to_this_item' => sprintf( esc_html__( 'Uploaded to this %s', 'wp-dispensary' ), $wpd_products_slug_singular ),
 		'items_list'            => sprintf( esc_html__( '%s list', 'wp-dispensary' ), $wpd_products_slug_cap ),
 		'items_list_navigation' => sprintf( esc_html__( '%s list navigation', 'wp-dispensary' ), $wpd_products_slug_cap ),
 		'filter_items_list'     => sprintf( esc_html__( 'Filter %s list', 'wp-dispensary' ), $wpd_products_slug ),
