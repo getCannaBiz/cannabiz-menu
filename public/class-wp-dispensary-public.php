75d3e1a55fb35aa707a205249e528a1a2c29acea
Robert DeVore <deviodigital@gmail.com>
2022-05-10 23:51:01 -0400

    :ok_hand: IMPROVE: Removed public .min CSS file

--- public/class-wp-dispensary-public.php
+++ public/class-wp-dispensary-public.php
@@ -64,7 +64,7 @@ class WP_Dispensary_Public {
 	 * @return void
 	 */
 	public function enqueue_styles() {
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/css/wp-dispensary-public.min.css', array(), $this->version, 'all' );
+		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'assets/css/wp-dispensary-public.css', array(), $this->version, 'all' );
 	}
 
 	/**
