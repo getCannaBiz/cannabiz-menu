8697461c5074bafa7096de972ee253511eaf3f4e
Robert DeVore <deviodigital@gmail.com>
2022-06-30 01:01:18 -0400

    :ok_hand: IMPROVE: General code cleanup

--- admin/class-wp-dispensary-vendors-taxonomy-fields.php
+++ admin/class-wp-dispensary-vendors-taxonomy-fields.php
@@ -152,22 +152,22 @@ if ( ! class_exists( 'WP_Dispensary_Vendor_Taxonomy_Fields' ) ) {
                 var _custom_media = true,
                 _orig_send_attachment = wp.media.editor.send.attachment;
                 $('body').on('click', button_class, function(e) {
-                var button_id = '#'+$(this).attr('id');
-                var send_attachment_bkp = wp.media.editor.send.attachment;
-                var button = $(button_id);
-                _custom_media = true;
-                wp.media.editor.send.attachment = function(props, attachment){
-                    if ( _custom_media ) {
-                    $('#vendor_logo').val(attachment.id);
-                    $('#vendor-logo-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
-                    $('#vendor-logo-wrapper .custom_media_image').attr('src',attachment.url).css('display','block');
-                    } else {
-                    return _orig_send_attachment.apply( button_id, [props, attachment] );
+                    var button_id = '#'+$(this).attr('id');
+                    var send_attachment_bkp = wp.media.editor.send.attachment;
+                    var button = $(button_id);
+                    _custom_media = true;
+                    wp.media.editor.send.attachment = function(props, attachment){
+                        if ( _custom_media ) {
+                        $('#vendor_logo').val(attachment.id);
+                        $('#vendor-logo-wrapper').html('<img class="custom_media_image" src="" style="margin:0;padding:0;max-height:100px;float:none;" />');
+                        $('#vendor-logo-wrapper .custom_media_image').attr('src',attachment.url).css('display','block');
+                        } else {
+                        return _orig_send_attachment.apply( button_id, [props, attachment] );
+                        }
                     }
-                }
-                wp.media.editor.open(button);
-                return false;
-            });
+                    wp.media.editor.open(button);
+                    return false;
+                });
             }
             ct_media_upload('.ct_tax_media_button.button'); 
             $('body').on('click','.ct_tax_media_remove',function(){
